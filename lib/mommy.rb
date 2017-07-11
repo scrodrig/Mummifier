33f6cad0d70be9fcbe9ecae4f554e10baebf728f
Schubert D. Rodríguez <scrodrig@thoughtworks.com>
2017-07-11 11:24:17 -0500

    Complete mummified excersice with TDD

--- lib/mommy.rb
+++ lib/mommy.rb
@@ -4,9 +4,15 @@ class Mommy
 
   def mummified(word_to_mummify)
     before_letter = word_to_mummify.chars[0]
+
+    if mummificable?(word_to_mummify)
+      return word_to_mummify
+    end
+
     word_to_mummify = word_to_mummify.chars.map do |letter|
       if !letter.chars.grep(REGEXP_VOWEL).empty?
-        before_letter = mummified_vowels(before_letter, letter)
+        mummified_vowels(before_letter, letter)
+        before_letter = MOMMY
       else
         before_letter = letter
       end
@@ -15,10 +21,14 @@ class Mommy
     return word_to_mummify
   end
 
+  def mummificable?(word_to_mummify)
+    total_vowels = word_to_mummify.chars.grep(REGEXP_VOWEL).size
+    return word_to_mummify.empty?  || total_vowels * 100 / word_to_mummify.chars.size < 30
+  end
+
   def mummified_vowels(before_letter, letter)
     if before_letter != MOMMY
       letter.gsub!(REGEXP_VOWEL, MOMMY)
-      return MOMMY
     else
       letter.gsub!(REGEXP_VOWEL, '')
     end
--- spec/mommy_spec.rb
+++ spec/mommy_spec.rb
@@ -2,9 +2,14 @@ require 'mommy'
 require 'faker'
 
 RSpec.describe Mommy, '#Mommy' do
+  MOMMY = 'mommy'
+  vowel = /[aeiouAEIOU]/
+  vowels = /[aeiouAEIOU]{2,5}/
+  consonant = /[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]/
+  consonants = /[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]{2,5}/
+
   it 'should not mummified an empty string' do
-    word_to_be_mummified = ''
-    mummified_word = Mommy.new.mummified(word_to_be_mummified)
+    mummified_word = Mommy.new.mummified('')
     expect(mummified_word).to be_empty
   end
 
@@ -14,25 +19,34 @@ RSpec.describe Mommy, '#Mommy' do
     expect(mummified_word).to eq(word_to_be_mummified)
   end
 
-  it 'Should mummified a single vowel letter' do
-    expected_word_mummified = 'mommy'
-    word_to_be_mummified = Faker::Base.regexify(/[aeiouAEIOU]/)
+  it 'should mummified a single vowel letter' do
+    expected_word_mummified = MOMMY
+    word_to_be_mummified = Faker::Base.regexify(vowel)
     mummified_word = Mommy.new.mummified(word_to_be_mummified)
     expect(mummified_word).to eq(expected_word_mummified)
   end
 
   it 'should mummified a single time when have two vowels together' do
-    expected_word_mummified = 'mommy'
-    word_to_be_mummified = Faker::Base.regexify(/[aeiouAEIOU][aeiouAEIOU]/)
+    expected_word_mummified = MOMMY
+    word_to_be_mummified = Faker::Base.regexify(vowels)
     mummified_word = Mommy.new.mummified(word_to_be_mummified)
     expect(mummified_word).to eq(expected_word_mummified)
   end
 
-  it 'should mummified a single time when have more than two vowels together' do
-    expected_word_mummified = 'mommy'
-    word_to_be_mummified = Faker::Base.regexify(/[aeiouAEIOU][aeiouAEIOU]/)
+  it 'should not mummified when the word has less than 30 percent of vowels' do
+    word_to_be_mummified = Faker::Base.regexify(consonants) + Faker::Base.regexify(vowel) + Faker::Base.regexify(consonants)
+    expected_word_mummified = word_to_be_mummified
     mummified_word = Mommy.new.mummified(word_to_be_mummified)
-    expect(mummified_word).to eq(expected_word_mummified)
+    expect(mummified_word).to eq(expected_word_mummified);
+  end
+
+  it 'should mummified when the word has more han 30 percent of vowels' do
+    consonant = Faker::Base.regexify(consonant)
+    vowel = Faker::Base.regexify(vowel)
+    word_to_be_mummified = consonant + vowel + consonant
+    expected_word_mummified = consonant + MOMMY + consonant
+    mummified_word = Mommy.new.mummified(word_to_be_mummified)
+    expect(mummified_word).to eq(expected_word_mummified);
   end
 
 end
